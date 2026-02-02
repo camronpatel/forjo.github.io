fd8028ef71e924e0703db8406cb399c4b6254eef
CamronPatel <patelcamron@gmail.com>
2026-02-02 12:46:13 -0500

    feat:yesBttn

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById('yesBtn').addEventListener('click', function() {
+  document.getElementById('danceGif').classList.remove('hidden');
+});
+
+document.getElementById('noBtn').addEventListener('click', function() {
+  const yesBtn = document.getElementById('yesBtn');
+  yesBtn.classList.add('big');
+  yesBtn.style.transform = `scale(${1.5 + (yesBtn.scale || 0)})`;
+  yesBtn.scale = (yesBtn.scale || 0) + 0.5;
+});
